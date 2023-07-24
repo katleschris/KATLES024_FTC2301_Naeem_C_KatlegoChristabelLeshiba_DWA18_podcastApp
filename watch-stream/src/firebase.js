7625cb07c7b3e169d318a87fef755ccd50a12d06
katleschris <106029638+katleschris@users.noreply.github.com>
2023-07-24 16:24:36 +0200

    initialised redux app

---
+++ watch-stream/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCZRcvbjOmBEw7-PosXzkWAj8dYt8qj9BQ",
+    authDomain: "watch-stream-site.firebaseapp.com",
+    projectId: "watch-stream-site",
+    storageBucket: "watch-stream-site.appspot.com",
+    messagingSenderId: "1010711354586",
+    appId: "1:1010711354586:web:3ddc393f9794a1bc0459e3",
+    measurementId: "G-M5NBS3M1NW"
+  };
