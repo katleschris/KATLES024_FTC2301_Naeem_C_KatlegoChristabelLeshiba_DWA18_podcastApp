bc6e0b2e62711caafe0e9c034f1f31861133c54c
katleschris <106029638+katleschris@users.noreply.github.com>
2023-07-24 23:32:22 +0200

    Implemented axios and TMDB API

--- watch-stream/src/Banner.js
+++ watch-stream/src/Banner.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Banner.css'
 import background5 from './background5.jpg'
+import axios from './axios';
+import requests from './Requests';
 
 function Banner() {
+
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData(){
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length -1)
+                ]
+            );
+            return request;
+        }
+
+        fetchData();
+
+    }, []);
+    
     function truncate(string, n){
         return string?.length > n? string.substr(0, n-1) + '...' : string
     }
-
 
   return (
     <header className='banner' 
