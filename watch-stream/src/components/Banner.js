082f0eb93c29e8af613f0a60de030e2cbd93d2f9
katleschris <106029638+katleschris@users.noreply.github.com>
2023-07-26 14:46:12 +0200

    Improved app structure

--- watch-stream/src/components/Banner.js
+++ watch-stream/src/components/Banner.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import './Banner.css'
 import background5 from './background5.jpg'
-import axios from './axios';
-import requests from './Requests';
+import axios from '../axios';
+import requests from '../Requests';
 
 function Banner() {
 
